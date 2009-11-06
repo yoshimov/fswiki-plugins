0695f40e00735d29cb53314b70568a19bf342882
NOMURA Yoshihide <nomura@pobox.com>
2009-11-06 15:59:43 -0800

    add various banner size.

--- googleadsense/Banner.pm
+++ googleadsense/Banner.pm
@@ -2,7 +2,12 @@
 #
 # Google AdSenseのBannerを表示する。
 # <pre>
-# {{googleadsense_banner AdSense ID,[h|v]}}
+# {{googleadsense_banner AdSense ID,slot ID,[h|v|l|b|w]}}
+# h .. 468x60
+# v .. 120x240
+# l .. 468x15
+# b .. 336x280
+# w .. 160x600
 # </pre>
 #
 ############################################################
@@ -18,42 +23,55 @@ sub inline {
 	my $self  = shift;
 	my $wiki  = shift;
 	my $userid = shift;
+	my $slotid = shift;
 	my $dir = shift;
 	my $buf = "";
 
 	if ($dir eq "") {
 	    $dir = "v";
 	}
-	if ($dir eq "v") {
-	    $buf .= <<"EOF";
+
+  $buf .= <<"EOF";
 <script type="text/javascript"><!--
 google_ad_client = "$userid";
+google_ad_slot = "$slotid";
+EOF
+  
+  if ($dir eq "v") {
+	    $buf .= <<"EOF";
 google_ad_width = 120;
 google_ad_height = 240;
-google_ad_format = "120x240_as";
-google_ad_type = "text";
-google_ad_channel ="";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
 EOF
-        } else {
+        } elsif ($dir eq "l") {
 	    $buf .= <<"EOF";
-<script type="text/javascript"><!--
-google_ad_client = "$userid";
+google_ad_width = 468;
+google_ad_height = 15;
+EOF
+        } elsif ($dir eq "b") {
+	    $buf .= <<"EOF";
+google_ad_width = 336;
+google_ad_height = 280;
+EOF
+        } elsif ($dir eq "w") {
+	    $buf .= <<"EOF";
+google_ad_width = 160;
+google_ad_height = 600;
+EOF
+  } else {
+	    $buf .= <<"EOF";
 google_ad_width = 468;
 google_ad_height = 60;
-google_ad_format = "468x60_as";
-google_ad_type = "text";
-google_ad_channel ="";
+EOF
+  }
+
+  $buf .= <<"EOF";
 //--></script>
 <script type="text/javascript"
   src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
 EOF
-        }
-	return $buf;
+
+  return $buf;
 }
 
 1;
